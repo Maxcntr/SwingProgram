e4d44e8a150915e29d1c48705c125e119924fb1e
vinni <>
2021-01-02 15:36:51 +0500

    hf

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,42 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main {
+public class Main implements ActionListener {
     public static void main(String[] args) {
+        Order();
+    }
 
-        JFrame jFrame = new JFrame("ЗАКАЗ ВЕЛОСИПЕДОВ");
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setSize(400,150);
+        public static void Order() {
 
-        JPanel jPanel = new JPanel();
-        JLabel jLabel = new JLabel("МОДЕЛЬ ВЕЛОСИПЕДА");
-        JTextField jTextField = new  JTextField(10);
-        JLabel jLabel1 = new JLabel("КОЛИЧЕСТВО ВЕЛОСИПЕДОВ");
-        JTextField jTextField1 = new JTextField(10);
-        JButton jButton = new JButton("ПОТВЕРДИТЬ ЗАКАЗ");
+            JFrame jFrame = new JFrame("ЗАКАЗ ВЕЛОСИПЕДОВ");
+            jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            jFrame.setSize(400, 150);
 
-        jPanel.add(jLabel);
-        jPanel.add(jTextField);
-        jPanel.add(jLabel1);
-        jPanel.add(jTextField1);
-        jPanel.add(jButton);
+            JPanel jPanel = new JPanel();
+            JLabel jLabel = new JLabel("МОДЕЛЬ ВЕЛОСИПЕДА");
+            JTextField jTextField = new JTextField(10);
+            JLabel jLabel1 = new JLabel("КОЛИЧЕСТВО ВЕЛОСИПЕДОВ");
+            JTextField jTextField1 = new JTextField(10);
+            JButton jButton = new JButton("ПОТВЕРДИТЬ ЗАКАЗ");
 
-        jFrame.getContentPane().add(BorderLayout.CENTER,jPanel);
+            jPanel.add(jLabel);
+            jPanel.add(jTextField);
+            jPanel.add(jLabel1);
+            jPanel.add(jTextField1);
+            jPanel.add(jButton);
 
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setResizable(false);
-        jFrame.setVisible(true);
+            jFrame.getContentPane().add(BorderLayout.CENTER, jPanel);
+
+            jFrame.setLocationRelativeTo(null);
+            jFrame.setResizable(false);
+            jFrame.setVisible(true);
+        }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
 
